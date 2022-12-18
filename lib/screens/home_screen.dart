748d09a595522033bfec521fea4d8ac8129999d0
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-12-18 18:53:55 +0530

    changes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -334,7 +334,7 @@ class _HomeScreenState extends State<HomeScreen> {
       //               },
       //               child: const Text('Camera'),
       //             ),
-
+//hiii
       //             ElevatedButton(
       //               onPressed: () {
       //                 BlocProvider.of<CameraCubit>(context)
